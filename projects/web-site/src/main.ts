f4d834d85038509969f1994609cc98587703591a
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-09 18:02:39 +0200

    pour forcer un commit avec amplify_output de l'autre projet

--- projects/web-site/src/main.ts
+++ projects/web-site/src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import outputs from '../../../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
+import outputs from '../../../amplify_outputs.json';
 
 Amplify.configure(outputs);
 const existingConfig = Amplify.getConfig();
