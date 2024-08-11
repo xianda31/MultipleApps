43e6254fff8709b7d21da83956b4a494596c88e1
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-11 08:47:09 +0200

    navbar ots

--- projects/web-site/src/app/app.component.ts
+++ projects/web-site/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminsComponent } from "./admins/admins.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminsComponent],
+  imports: [RouterOutlet, AdminsComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
