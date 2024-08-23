292da7cea193f35d831a6c1bf8d7f9e03b0ae183
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-23 18:25:23 +0200

    bug mineur

--- projects/admin-dashboard/src/app/home-page/home-page.component.ts
+++ projects/admin-dashboard/src/app/home-page/home-page.component.ts
@@ -36,7 +36,7 @@ export class HomePageComponent {
         title: 'Gestion du site',
         subtitle: 'Gestion du contenu du site ',
         description: 'Modifier le contenu des pages, des articles, des événements',
-        link1: { route: '/menus', label: 'menus et pages' }
+        link1: { route: '/layout', label: 'menus et pages' }
       },
       {
         title: 'Gestion des ventes',
