9413d3858030beb28d5c2803741b6d8001689460
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-25 13:59:37 +0200

    web-site css & home page

--- projects/web-site/src/app/app.routes.ts
+++ projects/web-site/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { TournamentsComponent } from './tournaments/tournaments.component';
 import { GenericSimplePageComponent } from './generic-simple-page/generic-simple-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 
 export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
     { path: 'tournaments', component: TournamentsComponent },
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: 'home' }
 ];
 
