ff4f3289169268b71226341d3c6cf0ada987eb5a
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-27 11:01:26 +0200

    mineur (trunk)

--- projects/common/pipes/trunk.pipe.ts
+++ projects/common/pipes/trunk.pipe.ts
@@ -1,5 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const MAX_LENGTH = 40;
+
 @Pipe({
     name: 'trunk',
     standalone: true
@@ -8,7 +10,10 @@ export class TrunkPipe implements PipeTransform {
 
     transform(string: string): string {
         let count = string.indexOf('|');
-        if (count === -1) return string;
+        if (count === -1) {
+            count = MAX_LENGTH;
+            console.log('No trunk found => ', string.slice(0, count));
+        }
         return string.slice(0, count);
     }
 
