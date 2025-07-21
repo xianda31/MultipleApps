dbea7a4cd05cc239f5356c1e6dc8af2d56dbb26b
xianda31 <99276316+xianda31@users.noreply.github.com>
2025-07-21 20:42:28 +0200

    gestion sympatisant

--- projects/web-back/src/app/members/members.component.ts
+++ projects/web-back/src/app/members/members.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MembersService } from './service/members.service';
 import { LicenseesService } from '../licensees/services/licensees.service';
-import { combineLatest, map, switchMap } from 'rxjs';
+import { combineLatest, map, switchMap, take, tap } from 'rxjs';
 import { Member } from '../../../../common/member.interface';
 import { FFB_licensee } from '../../../../common/ffb/interface/licensee.interface';
 import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -30,16 +30,14 @@ export class MembersComponent implements OnInit {
 
   // selected_filter: string = 'Tous';
   licenses_status: { [key: string]: string } = {
-    'paied': 'Réglée',
-    'unpaied': 'Non réglée',
-    'toutes': 'Toutes',
-    // 'offerte': 'Offerte',
+    'registered': 'Déclaré à la FFB',
+    'unregistered': 'Non déclaré à la FFB',
+    'all': 'Tout le répertoire BCSTO',
+    // 'offered': 'Offerte',
   };
 
   filters: string[] = ['Tous', ' de cette saison', ' non encore à jour'];
   selection: string = '';
-
-  verbose: string = '';
 
   radioButtonGroup: FormGroup = new FormGroup({
     radioButton: new FormControl('Tous')
@@ -57,215 +55,194 @@ export class MembersComponent implements OnInit {
 
   ngOnInit(): void {
     this.radioButtonGroup.valueChanges.subscribe(() => {
-      this.selection_filter();
+      this.onSelectLicence(this.radioButtonGroup.value.radioButton);
     });
+
     let today = new Date();
     this.season = this.sysConfService.get_season(today);
 
-    combineLatest([
-      this.membersService.listMembers(),
-      this.licenseesService.list_FFB_licensees$()
-    ])
-      .subscribe(([members, licensees]) => {
-        this.members = members;
+
+    this.licenseesService.list_FFB_licensees$().pipe(
+      tap((licensees) => {
         this.licensees = licensees;
-        this.check_licenses(this.season);
-
-        this.updateDBfromFFB();
-        this.filteredMembers = this.members;
-      }
-      );
+      }),
+      switchMap(() => this.membersService.listMembers()),
+      // take(1),
+      tap((members) => {
+        this.members = members;
+        this.reset_license_statuses();
+      }),
+    ).subscribe(() => {
+      this.filteredMembers = this.members;
+    });
   }
 
-  onSelectLicence(status:  string ) {
-    console.log('onSelectLicence', status);
 
-   
-      this.filteredMembers = this.members.filter((member: Member) => {
-        switch (status) {
-          case "all": //'Tous':
-            return member;
-          case "paied": //'à jour':
-            return (member.season === this.season) ? member : false;
-          case "unpaied": //'non à jour':
-            return (member.season !== this.season) ? member : false;
+
+  add_licensee(player: FFBplayer) {
+    if (player) {
+      const new_member: Member = this.player2member(player);
+      this.membersService.createMember(new_member);
+    }
+  }
+
+  add_newbee() {
+    const modalRef = this.modalService.open(GetNewbeeComponent, { centered: true });
+
+    modalRef.result.then((newbee: any) => {
+      if (newbee) {
+        let new_member: Member = {
+          id: '',
+          gender: newbee.gender,
+          firstname: newbee.firstname,
+          lastname: newbee.lastname.toUpperCase(),
+          license_number: '??' + newbee.lastname.toUpperCase().slice(0, 3) + newbee.firstname.slice(0, 3),
+          birthdate: newbee.birthdate,
+          city: this.capitalize_first(newbee.city.toLowerCase()),
+          season: '',
+          email: newbee.email ?? '',
+          phone_one: newbee.phone ?? '',
+          license_taken_at: 'BCSTO',
+          license_status: 'unregistred',
+          is_sympathisant: false,
         }
-        return member;
-      });
+        this.membersService.createMember(new_member).then((_member) => {
+          this.toastService.showSuccess('Nouveau membre non licencié', new_member.lastname + ' ' + new_member.firstname);
+        });
+      }
+
+    });
   }
 
-  async check_licenses(season: string) {
-      for (const member of this.members) {
-        if (member.season !== season && member.license_status !== 'unpaied') {
-          member.license_status = 'unpaied';
-          await this.membersService.updateMember(member);
+  capitalize_first(str: string | undefined): string {
+    if (!str) return '';
+    str = str.replace(/^(\w)(.+)/, (match, p1, p2) => p1.toUpperCase() + p2.toLowerCase())
+    return str;
+  }
+
+  player2member(player: FFBplayer): Member {
+
+    return {
+      id: '',
+      gender: player.gender === 1 ? 'M.' : 'Mme',
+      firstname: player.firstname,
+      lastname: player.lastname.toUpperCase(),
+      license_number: player.license_number,
+      birthdate: player.birthdate,
+      city: this.capitalize_first(player.city.toLowerCase()),
+      season: player.last_season,
+      email: '?',
+      phone_one: '?',
+      license_taken_at: player.last_club ?? '??',
+      license_status: player.is_current_season ? 'duly_registered' : 'unregistered',
+      is_sympathisant: false,
+    }
+  }
+
+  updateDBfromFFB() {
+    this.licensees.forEach((licensee) => {
+      this.createOrUpdateMember(licensee);
+    });
+  }
+
+  async reset_license_statuses() {
+    for (const member of this.members) {
+      if (!this.licensees.some((l) => l.license_number === member.license_number)) {
+        if (member.license_status !== 'unregistered') {
+          member.license_status = 'unregistered';
+          this.membersService.updateMember(member);
           this.toastService.showWarning('Licences', `Licence de ${member.lastname} ${member.firstname} obsolète `);
         }
       }
     }
+  }
 
-    add_licensee(player: FFBplayer) {
-      if (player) {
-        const new_member: Member = this.player2member(player);
-        this.membersService.createMember(new_member);
+
+  onSelectLicence(status: string) {
+    this.filteredMembers = this.members.filter((member: Member) => {
+      switch (status) {
+        case "registered": //'à jour':
+          return (member.license_status === 'duly_registered' || member.license_status === 'promoted_only');
+        case "unregistered": //'non à jour':
+          return (member.license_status === 'unregistered');
+        case "all": //'Tous':
+        default: //'Tous':
+          return true;
       }
-    }
-
-    add_newbee() {
-      const modalRef = this.modalService.open(GetNewbeeComponent, { centered: true });
-
-      modalRef.result.then((newbee: any) => {
-        if (newbee) {
-          let new_member: Member = {
-            id: '',
-            gender: newbee.gender,
-            firstname: newbee.firstname,
-            lastname: newbee.lastname.toUpperCase(),
-            license_number: '??' + newbee.lastname.toUpperCase().slice(0, 3) + newbee.firstname.slice(0, 3),
-            birthdate: newbee.birthdate,
-            city: this.capitalize_first(newbee.city.toLowerCase()),
-            season: '',
-            email: newbee.email ?? '',
-            phone_one: newbee.phone ?? '',
-            license_taken_at: 'BCSTO',
-            license_status: 'unknown',
-            is_sympathisant: false,
-          }
-          this.membersService.createMember(new_member).then((_member) => {
-            this.toastService.showSuccess('Nouveau membre non licencié', new_member.lastname + ' ' + new_member.firstname);
-          });
-        }
-
-      });
-    }
-
-    capitalize_first(str: string | undefined): string {
-      if (!str) return '';
-      str = str.replace(/^(\w)(.+)/, (match, p1, p2) => p1.toUpperCase() + p2.toLowerCase())
-      return str;
-    }
-
-
-
-    player2member(player: FFBplayer): Member {
-
-      return {
-        id: '',
-        gender: player.gender === 1 ? 'M.' : 'Mme',
-        firstname: player.firstname,
-        lastname: player.lastname.toUpperCase(),
-        license_number: player.license_number,
-        birthdate: player.birthdate,
-        city: this.capitalize_first(player.city.toLowerCase()),
-        season: player.last_season,
-        email: '?',
-        phone_one: '?',
-        license_taken_at: player.last_club ?? '??',
-        license_status: player.is_current_season ? 'paied' : 'unknown',
-        is_sympathisant: false,
-      }
-    }
-
-    updateDBfromFFB() {
-      this.verbose = '';
-      this.licensees.forEach((licensee) => {
-        this.createOrUpdateMember(licensee);
-      });
-      if (this.verbose.length === 0) {
-        this.verbose = '.. la base adhérents est à jour (aucune modification) ';
-      }
-    }
-
-    selection_filter() {
-      this.selection = this.radioButtonGroup.value.radioButton;
-      this.filteredMembers = this.members.filter((member: Member) => {
-        switch (this.selection) {
-          case this.filters[0]: //'Tous':
-            return member;
-          case this.filters[1]: //'à jour':
-            return (member.season === this.season) ? member : false;
-          case this.filters[2]: //'non à jour':
-            return (member.season !== this.season) ? member : false;
-        }
-        return member;
-      });
-    }
-
-
+    });
+  }
 
   async createOrUpdateMember(licensee: FFB_licensee) {
-      let existingMember = this.members.find((m) => m.license_number === licensee.license_number);
+    let existingMember = this.members.find((m) => m.license_number === licensee.license_number);
 
-      if (existingMember) {
-        let member = this.compare(existingMember, licensee);
-        if (member !== null) {
-          // this.verbose += 'modification : ' + member.lastname + ' ' + member.firstname + '\n';
-          console.log('%s updated to %s', existingMember.lastname, member.lastname);
-          await this.membersService.updateMember(member);
-        }
-
-      } else {
-        // console.log('MembersComponent.createOrUpdateMember create');
-        let newMember = this.createNewMember(licensee);
-        this.verbose += 'creation : ' + newMember.lastname + ' ' + newMember.firstname + '\n';
-        let new_member = await this.membersService.createMember(newMember);
+    if (existingMember) {
+      let member = this.compare(existingMember, licensee);
+      if (member !== null) {
+        // this.verbose += 'modification : ' + member.lastname + ' ' + member.firstname + '\n';
+        console.log('%s updated to %s', existingMember.lastname, member.lastname);
+        await this.membersService.updateMember(member);
       }
+
+    } else {
+      // console.log('MembersComponent.createOrUpdateMember create');
+      let newMember = this.createNewMember(licensee);
+      await this.membersService.createMember(newMember);
     }
+  }
 
 
-    compare(member: Member, licensee: FFB_licensee): Member | null {
+  compare(member: Member, licensee: FFB_licensee): Member | null {
 
-      let nextMember: Member = {
-        id: member.id,
-        license_number: licensee.license_number,
-        gender: licensee.gender,
-        firstname: licensee.firstname,
-        lastname: licensee.lastname.toUpperCase(),
-        birthdate: licensee.birthdate,
-        city: this.capitalize_first(licensee.city?.toLowerCase()),
-        season: licensee.season ?? (licensee.register ? this.season : ''),
-        email: licensee.email?.trim().toLowerCase() ?? '',
-        phone_one: licensee.phone_one,
-        license_taken_at: licensee.orga_license_name ?? 'BCSTO',
-        license_status: licensee.license_id ? (licensee.free ? 'offerte' : 'paied') : 'unpaied',
-        is_sympathisant: licensee.is_sympathisant ?? false,
+    let nextMember: Member = {
+      id: member.id,
+      license_number: licensee.license_number,
+      gender: licensee.gender,
+      firstname: licensee.firstname,
+      lastname: licensee.lastname.toUpperCase(),
+      birthdate: licensee.birthdate,
+      city: this.capitalize_first(licensee.city?.toLowerCase()),
+      season: (licensee.season || licensee.license_id) ? this.season : '',
+      email: licensee.email?.trim().toLowerCase() ?? '',
+      phone_one: licensee.phone_one,
+      license_taken_at: licensee.orga_license_name ?? 'BCSTO',
+      license_status: licensee.register ? (licensee.license_id ? 'duly_registered' : 'promoted_only') : 'unregistered',
+      is_sympathisant: licensee.is_sympathisant ?? false,
 
-
-      }
-      let is: { [key: string]: any } = member;
-      let next: { [key: string]: any } = nextMember;
-      let diff: boolean = false;
-
-      for (let key in next) {
-        if (next[key] !== is[key]) {
-          this.verbose += 'updating ' + member.lastname + ' ' + member.firstname + ' (' + key + ')' + '\n';
-          diff = true;
-        }
-      }
-      return diff ? nextMember : null;
-    }
-
-    createNewMember(licensee: FFB_licensee): Member {
-      return {
-        id: '',
-        gender: licensee.gender,
-        firstname: licensee.firstname,
-        lastname: licensee.lastname.toUpperCase(),
-        license_number: licensee.license_number,
-        birthdate: licensee.birthdate,
-        city: this.capitalize_first(licensee.city?.toLowerCase()),
-        season: licensee.season ?? (licensee.register ? this.season : ''),
-        email: licensee.email ?? '',
-        phone_one: licensee.phone_one,
-        is_sympathisant: licensee.is_sympathisant ?? false,
-        license_status: licensee.license_id ? (licensee.free ? 'offerte' : 'réglée') : 'à payer',
-        license_taken_at: licensee.orga_license_name ?? 'BCSTO',
-      }
 
     }
-    deleteMember(member: Member) {
-      this.membersService.deleteMember(member);
+    let is: { [key: string]: any } = member;
+    let next: { [key: string]: any } = nextMember;
+    let diff: boolean = false;
+
+    for (let key in next) {
+      if (next[key] !== is[key]) {
+        diff = true;
+      }
+    }
+    return diff ? nextMember : null;
+  }
+
+  createNewMember(licensee: FFB_licensee): Member {
+    return {
+      id: '',
+      gender: licensee.gender,
+      firstname: licensee.firstname,
+      lastname: licensee.lastname.toUpperCase(),
+      license_number: licensee.license_number,
+      birthdate: licensee.birthdate,
+      city: this.capitalize_first(licensee.city?.toLowerCase()),
+      season: licensee.season ?? (licensee.register ? this.season : ''),
+      email: licensee.email ?? '',
+      phone_one: licensee.phone_one,
+      is_sympathisant: licensee.is_sympathisant ?? false,
+      license_status: licensee.register ? (licensee.license_id ? 'duly_registered' : 'promoted_only') : 'unregistered',
+      license_taken_at: licensee.orga_license_name ?? 'BCSTO',
     }
 
   }
+  deleteMember(member: Member) {
+    this.membersService.deleteMember(member);
+  }
+
+}
 
