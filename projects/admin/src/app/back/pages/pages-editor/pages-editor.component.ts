e06bb330c1e47f6642d891b00bfffb1b7914b862
xianda31 <99276316+xianda31@users.noreply.github.com>
2025-08-22 20:05:10 +0200

    editeur pages avec create et trash

--- projects/admin/src/app/back/pages/pages-editor/pages-editor.component.ts
+++ projects/admin/src/app/back/pages/pages-editor/pages-editor.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, Signal, signal } from '@angular/core';
 import { PageService } from '../../../common/services/page.service';
 import { SnippetService } from '../../../common/services/snippet.service';
 import { MENU_TITLES, Page, PAGE_TEMPLATES, Snippet } from '../../../common/interfaces/page_snippet.interface';
 import { CommonModule } from '@angular/common';
-import { Form, FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastService } from '../../../common/services/toast.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { combineLatest, map, Observable, switchMap, take } from 'rxjs';
+import { combineLatest, map, Observable, take, timer } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SnippetModalEditorComponent } from '../../site/snippet-modal-editor/snippet-modal-editor.component';
 import { FileService } from '../../../common/services/files.service';
-
+import {CdkDrag,CdkDropList, CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 
 @Component({
   selector: 'app-pages-editor',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgbModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgbModule, CdkDrag,CdkDropList],
   templateUrl: './pages-editor.component.html',
   styleUrl: './pages-editor.component.scss'
 })
@@ -25,8 +25,14 @@ export class PagesEditorComponent {
   selected_snippet: Snippet | null = null;
   selected_snippet_image: string = '';
   selected_snippet_file: string = '';
+
+  view_snippet = signal<boolean>(true);
+  freeze_view_snippet = signal<boolean>(false);
+
   pagesForm !: FormGroup;
   pageTemplates = Object.values(PAGE_TEMPLATES);
+
+  newSnippetForm !: FormGroup;
 
   file_paths$ !: Observable<string[]>;
   thumbnails$ !: Observable<string[]>;
@@ -45,6 +51,15 @@ export class PagesEditorComponent {
   }
 
   ngOnInit(): void {
+
+    this.newSnippetForm = this.fb.group({
+      title: ['', Validators.required],
+      subtitle: ['', Validators.required],
+      content: [''],
+      image_url: [''],
+      file: [''],
+      public: [true]
+    });
 
     this.file_paths$ = this.fileService.list_files('documents/').pipe(
       map((S3items) => S3items.map(item => item.path))
@@ -79,7 +94,7 @@ export class PagesEditorComponent {
           this.snippets = snippets;
           this.pages.forEach(page => {
             page.snippets = this.getPageSnippets(page);
-            page.snippet_ids = page.snippets.map(snippet => snippet.id);
+            // page.snippet_ids = page.snippets.map(snippet => snippet.id);
           });
           this.initFormArray(this.pages);
         });
@@ -112,8 +127,9 @@ export class PagesEditorComponent {
 
 
   getPageSnippets(page: Page): Snippet[] {
-    return this.snippets.filter(snippet => snippet.template === page.title);
+    return page.snippet_ids.map(id => this.snippets.find(snippet => snippet.id === id)).filter((s): s is Snippet => s !== undefined);
   }
+
 
   savePage(pageGroup: FormGroup): void {
 
@@ -136,16 +152,50 @@ export class PagesEditorComponent {
     }
   }
 
-  onSnippetClick(snippet: Snippet) {
-    // this.openSnippetEditorCanvas();
+  addSnippet(pageGroup: FormGroup) {
+    if (this.newSnippetForm.valid) {
+      const snippet: Snippet = {
+        id: '',
+        title: this.newSnippetForm.get('title')?.value,
+        subtitle: this.newSnippetForm.get('subtitle')?.value,
+        content: '<p>Nouvelle vignette</p>',
+        file: this.newSnippetForm.get('file')?.value,
+        image: this.newSnippetForm.get('image_url')?.value,
+        public: this.newSnippetForm.get('public')?.value,
+
+        template: 'n/a',
+        rank: 'n/a',
+      };
+      this.snippetService.createSnippet(snippet).then((new_snippet) => {
+        this.toastService.showSuccess('Snippets', 'Snippet créé avec succès');
+        pageGroup.get('snippets')?.value.push(new_snippet);
+        pageGroup.get('snippet_ids')?.value.push(new_snippet.id);
+        this.savePage(pageGroup);
+        this.newSnippetForm.reset();
+      });
+    }
   }
 
+  onSnippetClick(snippet: Snippet) {
+    this.selected_snippet = snippet || null;
+    this.freeze_view_snippet.set(true);
+  }
+
+  toggleFreezeViewSnippet() {
+    this.freeze_view_snippet.set(!this.freeze_view_snippet());
+  }
 
   onHover(snippet?: Snippet) {
-    // this.selected_snippet_image = snippet?.image || '';
-    // this.selected_snippet_file = snippet?.file || '';
-    this.selected_snippet = snippet || null;
+    if(this.selected_snippet?.id !== snippet?.id && !this.freeze_view_snippet()) {
+      this.selected_snippet = snippet || null;
+      // armer un timer qui etendra la visibilité du snippet
+      this.view_snippet.set(true);
+      setTimeout(() => {
+        this.view_snippet.set(false);
+      }, 5000);
+    }
   }
+
 
   onSnippetContentClick(snippet: Snippet) {
     const modalRef = this.modalService.open(SnippetModalEditorComponent, { centered: true });
@@ -204,5 +254,32 @@ export class PagesEditorComponent {
             console.error('Error updating snippet:', error);
           });
       }
+    }
+
+    // drag and drop handling
+
+    dropSnippet(event: CdkDragDrop<Snippet[]>, pageIndex: number) {
+      const snippets = this.pageGroups[pageIndex].get('snippets')?.value;
+      if (snippets) {
+        moveItemInArray(snippets, event.previousIndex, event.currentIndex);
+        this.pageGroups[pageIndex].get('snippets')?.setValue(snippets);
+        this.updateSnippetList(pageIndex);
+      }
+    }
+
+    binSnippet(event: CdkDragDrop<Snippet[]>, pageIndex: number) {
+      const snippets = this.pageGroups[pageIndex].get('snippets')?.value;
+      if (snippets) {
+        const removed = snippets.splice(event.previousIndex, 1);
+        this.pageGroups[pageIndex].get('snippets')?.setValue(snippets);
+        this.updateSnippetList(pageIndex);
+      }
+    }
+
+    updateSnippetList(pageIndex: number) {
+      const snippets = this.pageGroups[pageIndex].get('snippets')?.value;
+      const snippet_ids = snippets.map((s: Snippet) => s.id);
+      this.pageGroups[pageIndex].get('snippet_ids')?.setValue(snippet_ids);
+      this.savePage(this.pageGroups[pageIndex]);
     }
   }
