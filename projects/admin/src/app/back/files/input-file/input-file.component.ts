e2efbf994aded583cc6ea210b9c382c6bacef8da
xianda31 <99276316+xianda31@users.noreply.github.com>
2025-09-06 08:39:11 +0200

    intermediaire 3

--- projects/admin/src/app/back/files/input-file/input-file.component.ts
+++ projects/admin/src/app/back/files/input-file/input-file.component.ts
@@ -19,69 +19,70 @@ import { CommonModule } from '@angular/common';
 export class InputFileComponent implements ControlValueAccessor {
   @Input() fileSystemNode !: FileSystemNode;
   input!: string;
+  placeholder: string = 'Sélectionner un fichier';
+  disabled = false;
 
   current_node_childs = signal<FileSystemNode | null>(null);
   node_stack: FileSystemNode[] = [];
   returned_value: any;
 
-  onChange: (value: string | null) => void = () => {
-  };
+  onChange: (value: S3Item | null) => void = () => { };
   onTouch: () => void = () => { };
-  disabled = false;
 
   constructor() { }
   ngOnInit(): void {
     if (!this.fileSystemNode) return;
     this.open_node(this.fileSystemNode);
     this.node_stack.push(this.fileSystemNode);
+    console.log('initial stack:', this.node_stack);
+    this.placeholder = 'Sélectionner un fichier dans ' + Object.keys(this.fileSystemNode)[0];
   }
 
   writeValue(input: S3Item | null): void {
-    if (input === undefined || input === null) {
-      this.input = '';
-      return;
-    }
-
-    this.input = input.path;
+    this.input = (input === undefined || input === null) ? '' : input.path;
   }
+
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any): void {
     this.onTouch = fn;
   }
+
   setDisabledState?(isDisabled: boolean): void {
     this.disabled = isDisabled;
   }
 
-  setValue(key:string) {
+  setValue(key: string) {
     this.onTouch();
     console.log('setValue', key)
     if (this.current_node_childs() === null) return;
-    
+
     // search input in current_node_childs
     const childs = this.current_node_childs() as FileSystemNode;
     const found = childs[key]?.['__data'] as unknown as S3Item;
-    console.log('found', found)
-    if (found && found.size > 0) {
-      this.input = found.path;
+    // console.log('found', found)
+    if (found.size > 0) {
+      // is a file
+      this.input = key;
       this.returned_value = found;
-      this.onChange(found.path);
+      this.onChange(found);
     } else {
       // not a file
       const node: FileSystemNode = { [key]: childs[key] };
-
-      console.log('not a file, open it', node)
-      this.open_node(node);
+      this.input = '';
+      this.placeholder = 'Sélectionner un fichier dans ' + found.path;
+      this.push_node(key, childs[key]);
+      // this.open_node(node);
     }
-    // this.onChange(found);
   }
 
   // filesystem utilities
 
-  select_node(node: FileSystemNode) {
-    console.log('selected', node)
-  }
+  // select_node(node: FileSystemNode) {
+  //   console.log('selected', node)
+  // }
 
 
   open_node(node: FileSystemNode): void {
@@ -93,15 +94,20 @@ export class InputFileComponent implements ControlValueAccessor {
     // regenerate FileSystemNode from html (loop on current_node_childs() | keyvalue)
     const node: FileSystemNode = { [key]: value };
     this.open_node(node)
-    if (isFolder(node)) this.node_stack.push(node);
+    // if (isFolder(node)) { 
+    this.node_stack.push(node);
+    // console.log('Pushing node, new current:', node);
+    // }
   }
 
   pop_node() {
-    this.node_stack.pop();
-    const parent = this.node_stack[this.node_stack.length - 1];
-    if (parent === null) return;
-    console.log('Popping node, new current:', parent);
-    this.open_node(parent);
+    if (this.node_stack.length > 1) {
+      this.node_stack.pop();
+      const prevNode = this.node_stack[this.node_stack.length - 1];
+      this.open_node(prevNode);
+      this.input = '';
+      this.placeholder = 'Sélectionner un fichier dans ' + Object.keys(prevNode)[0];
+    }
   }
 
   get_icon(node: any): string {
