c93beb41f26ff921005037b3f6fbe34a4a05595d
xianda31 <99276316+xianda31@users.noreply.github.com>
2025-11-06 09:24:15 +0100

    simplification app routing

--- projects/admin/src/app/app.component.ts
+++ projects/admin/src/app/app.component.ts
@@ -1,43 +1,15 @@
-import { registerLocaleData, CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterModule, ActivatedRoute } from '@angular/router';
-import { NavigationEnd } from '@angular/router';
-import localeFr from '@angular/common/locales/fr';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LocalStorageService } from './back/services/local-storage.service';
+import { RouterModule } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, ReactiveFormsModule, CommonModule, FormsModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(
-    private localStorageService: LocalStorageService,
-    private router: Router,
-    private route: ActivatedRoute,
-  ) { }
-
-  ngOnInit(): void {
-
-    registerLocaleData(localeFr);
-
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        const segments = this.router.url.split('/').filter(Boolean); // Segments réels de l'URL
-        // Si l'URL est vide => rediriger vers l'entrée point
-        if (segments.length === 0) {
-          let entry_point = this.localStorageService.getItem('entry_point');
-          entry_point = entry_point ? entry_point : 'front';
-          this.router.navigate([entry_point], { relativeTo: this.route });
-        }else{
-          // console.log('Non empty URL, no redirection:', segments);
-        }
-      };
-    });
-
-  }
+  constructor() {}
 }
