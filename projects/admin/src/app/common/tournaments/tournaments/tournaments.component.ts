e62f720c662549f8e23ab1d7351c2d1df15e45a5
xianda31 <99276316+xianda31@users.noreply.github.com>
2026-01-01 09:38:45 +0100

    UI mineurs (homepage, tournaments)

--- projects/admin/src/app/common/tournaments/tournaments/tournaments.component.ts
+++ projects/admin/src/app/common/tournaments/tournaments/tournaments.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { TournamentService } from '../../services/tournament.service';
 import { TournamentTeams } from '../../ffb/interface/tournament_teams.interface';
