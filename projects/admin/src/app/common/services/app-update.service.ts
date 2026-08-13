9376c8a32b4cde50d7feb441d4a0608fe5deafd8
xianda31 <99276316+xianda31@users.noreply.github.com>
2026-08-13 12:12:02 +0200

    TEK : log [AppUpdate]

--- projects/admin/src/app/common/services/app-update.service.ts
+++ projects/admin/src/app/common/services/app-update.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,48 @@ import { SwUpdate } from '@angular/service-worker';
 export class AppUpdateService {
   private updateInProgress = false;
 
-  constructor(private swUpdate: SwUpdate) { }
+  constructor(private swUpdate: SwUpdate) {
+    this.swUpdate.versionUpdates.subscribe((event) => {
+      switch (event.type) {
+        case 'VERSION_DETECTED':
+          console.info('[AppUpdate] New build detected', {
+            currentBuild: environment.buildInfo,
+            detectedVersionHash: event.version.hash,
+          });
+          break;
+        case 'VERSION_READY':
+          console.info('[AppUpdate] New build ready', {
+            currentVersionHash: event.currentVersion.hash,
+            latestVersionHash: event.latestVersion.hash,
+          });
+          break;
+        case 'NO_NEW_VERSION_DETECTED':
+          console.info('[AppUpdate] Current build is up to date', {
+            currentBuild: environment.buildInfo,
+            versionHash: event.version.hash,
+          });
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.error('[AppUpdate] New build installation failed', {
+            versionHash: event.version.hash,
+            error: event.error,
+          });
+          break;
+      }
+    });
+  }
 
   async checkAtStartup(): Promise<void> {
     if (!this.swUpdate.isEnabled || this.updateInProgress) {
+      console.info('[AppUpdate] Update check skipped', {
+        serviceWorkerEnabled: this.swUpdate.isEnabled,
+        updateInProgress: this.updateInProgress,
+      });
       return;
     }
 
     try {
+      console.info('[AppUpdate] Checking for a new build at startup', environment.buildInfo);
       await navigator.serviceWorker.ready;
       const updateReady = await this.swUpdate.checkForUpdate();
 
@@ -23,6 +58,7 @@ export class AppUpdateService {
       }
 
       this.updateInProgress = true;
+  console.info('[AppUpdate] Activating new build and reloading');
       await this.swUpdate.activateUpdate();
       window.location.reload();
     } catch (error) {
