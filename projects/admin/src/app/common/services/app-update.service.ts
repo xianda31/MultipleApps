8e7db65ce9e1947f5f3bbda7bd5b794db3d88439
xianda31 <99276316+xianda31@users.noreply.github.com>
2026-08-13 12:22:57 +0200

    TEK : correction console.log de app-update.service

--- projects/admin/src/app/common/services/app-update.service.ts
+++ projects/admin/src/app/common/services/app-update.service.ts
@@ -6,25 +6,28 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root'
 })
 export class AppUpdateService {
+  private readonly activatedBuildKey = 'app-update-activated-build';
   private updateInProgress = false;
 
   constructor(private swUpdate: SwUpdate) {
+    this.logPreviousActivation();
+
     this.swUpdate.versionUpdates.subscribe((event) => {
       switch (event.type) {
         case 'VERSION_DETECTED':
-          console.info('[AppUpdate] New build detected', {
+          console.log('[AppUpdate] New build detected', {
             currentBuild: environment.buildInfo,
             detectedVersionHash: event.version.hash,
           });
           break;
         case 'VERSION_READY':
-          console.info('[AppUpdate] New build ready', {
+          console.log('[AppUpdate] New build ready', {
             currentVersionHash: event.currentVersion.hash,
             latestVersionHash: event.latestVersion.hash,
           });
           break;
         case 'NO_NEW_VERSION_DETECTED':
-          console.info('[AppUpdate] Current build is up to date', {
+          console.log('[AppUpdate] Current build is up to date', {
             currentBuild: environment.buildInfo,
             versionHash: event.version.hash,
           });
@@ -41,7 +44,7 @@ export class AppUpdateService {
 
   async checkAtStartup(): Promise<void> {
     if (!this.swUpdate.isEnabled || this.updateInProgress) {
-      console.info('[AppUpdate] Update check skipped', {
+      console.log('[AppUpdate] Update check skipped', {
         serviceWorkerEnabled: this.swUpdate.isEnabled,
         updateInProgress: this.updateInProgress,
       });
@@ -49,7 +52,7 @@ export class AppUpdateService {
     }
 
     try {
-      console.info('[AppUpdate] Checking for a new build at startup', environment.buildInfo);
+      console.log('[AppUpdate] Checking for a new build at startup', environment.buildInfo);
       await navigator.serviceWorker.ready;
       const updateReady = await this.swUpdate.checkForUpdate();
 
@@ -58,11 +61,28 @@ export class AppUpdateService {
       }
 
       this.updateInProgress = true;
-  console.info('[AppUpdate] Activating new build and reloading');
+      console.log('[AppUpdate] Activating new build and reloading');
+      sessionStorage.setItem(this.activatedBuildKey, JSON.stringify({
+        previousBuild: environment.buildInfo,
+        activatedAt: new Date().toISOString(),
+      }));
       await this.swUpdate.activateUpdate();
       window.location.reload();
     } catch (error) {
       console.warn('[AppUpdate] Startup update check failed', error);
     }
+  }
+
+  private logPreviousActivation(): void {
+    const activation = sessionStorage.getItem(this.activatedBuildKey);
+    if (!activation) {
+      return;
+    }
+
+    sessionStorage.removeItem(this.activatedBuildKey);
+    console.log('[AppUpdate] New build loaded after automatic reload', {
+      currentBuild: environment.buildInfo,
+      activation: JSON.parse(activation),
+    });
   }
 }
